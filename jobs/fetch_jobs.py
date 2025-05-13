2fa35d8d72f23196d7fe15c42de57b2239d14606
remotrek <remotrekoficial@gmail.com>
2025-05-13 15:36:09 -0400

    Update fetch_jobs.py

--- jobs/fetch_jobs.py
+++ jobs/fetch_jobs.py
@@ -22,7 +22,7 @@ try:
     cred = credentials.Certificate({
         "type": "service_account",
         "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
         "client_email": os.getenv("FIREBASE_CLIENT_EMAIL")
     })
     initialize_app(cred)
